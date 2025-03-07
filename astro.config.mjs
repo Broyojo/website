b1a674a2e8d23b8cc5f43ee2615702f0856b54eb
Broyojo <dha@xoba.com>
2025-03-07 10:05:38 -0500

    added simple blog system and added test post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+        shikiConfig: {
+            theme: "dark-plus",
+            wrap: true,
+            defaultColor: false,
+        },
+
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    }
+});
